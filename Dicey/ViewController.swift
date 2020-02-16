d057a13d4690c78993bcd9f4b69bb6272eef4779
Kamy <kaminderpal@yahoo.com>
2020-02-15 20:00:09 -0500

    Initial Commit

---
+++ Dicey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicey
+//
+//  Created by Kaminderpal Singh on 2020-02-15.
+//  Copyright © 2020 Kaminderpal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
